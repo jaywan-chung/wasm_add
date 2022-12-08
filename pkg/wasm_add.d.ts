2c4a2cf6d2fa8b755a450c4e4d9b8495bb6cc588
jaywan-chung <64951418+jaywan-chung@users.noreply.github.com>
2022-12-08 21:28:01 +0900

    Add files via upload

---
+++ pkg/wasm_add.d.ts
@@ -0,0 +1,28 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+* @param {Float64Array} a
+* @param {Float64Array} b
+* @returns {Float64Array}
+*/
+export function wasm_add(a: Float64Array, b: Float64Array): Float64Array;
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+  readonly memory: WebAssembly.Memory;
+  readonly wasm_add: (a: number, b: number, c: number, d: number, e: number) => void;
+  readonly __wbindgen_add_to_stack_pointer: (a: number) => number;
+  readonly __wbindgen_malloc: (a: number) => number;
+  readonly __wbindgen_free: (a: number, b: number) => void;
+}
+
+/**
+* If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+* for everything else, calls `WebAssembly.instantiate` directly.
+*
+* @param {InitInput | Promise<InitInput>} module_or_path
+*
+* @returns {Promise<InitOutput>}
+*/
+export default function init (module_or_path?: InitInput | Promise<InitInput>): Promise<InitOutput>;
